d1c255a2b62caa2e806a02f890582eaaccccb061
realdezzy <realdezzy1@gmail.com>
2023-07-05 00:28:41 +0100

    Error type correction

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -10,13 +10,12 @@
  * @returns {Map} - Map with values.
  */
 export default function updateUniqueItems(map) {
-    if (! map instanceof Map) throw new TypeError("Cannot process");
-    
-    map.forEach((value, key, map) => {
+    if (!( map instanceof Map))
+        throw new Error("Cannot process");
+
+    map.forEach((value, key) => {
         if (value == 1){
             map.set(key, 100);
         }
     })
-    
-    return map;
 }
