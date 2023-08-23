7641f4b712d430fd151f716df5977aa8b8ad92e1
realdezzy <realdezzy1@gmail.com>
2023-08-23 02:15:08 +0100

    task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,10 +1,16 @@
 #!/usr/bin/env node
-const app = require('http');
+const http = require('http');
 
-// create a server object:
-app.createServer(function (req, res) {
-  res.write('Hello Holberton School!'); // write a response to the client
-  res.end(); // end the response
-}).listen(1245); // the server object listens on port 1245
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end('Hello Holberton School!');
+});
+
+app.listen(port, hostname, () => {
+});
 
 module.exports = app;
